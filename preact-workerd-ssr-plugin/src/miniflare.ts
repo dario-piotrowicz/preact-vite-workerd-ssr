87900befeb4268c9db5f1476b5f32dbeb0be3401
Dario Piotrowicz <dario@cloudflare.com>
2023-10-31 19:15:22 +0000

    remove unnecessary unsafeEvalBinding argument

--- preact-workerd-ssr-plugin/src/miniflare.ts
+++ preact-workerd-ssr-plugin/src/miniflare.ts
@@ -2,15 +2,13 @@ import { Log, Miniflare } from "miniflare";
 
 export function createMiniflareInstance({
   script,
-  unsafeEvalBinding,
 }: {
   script: string;
-  unsafeEvalBinding: string;
 }): Miniflare {
   return new Miniflare({
     log: new Log(),
     modules: true,
     script,
-    unsafeEvalBinding
+    unsafeEvalBinding: "UNSAFE_EVAL"
   });
 }
