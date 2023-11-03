13c1369328caa8b644a036bc734d476dbf7344aa
Dario Piotrowicz <dario@cloudflare.com>
2023-11-03 16:38:42 +0000

    make sure that middleware is instantiated as soon as possible

--- workerd-vite-utils/src/index.ts
+++ workerd-vite-utils/src/index.ts
@@ -3,6 +3,7 @@ import { ViteDevServer } from "vite";
 import { createMiniflareInstance } from "./miniflare.js";
 //@ts-ignore
 import workerdBootloader from "./workerdBootloader.js.txt";
+import type { Miniflare } from "miniflare";
 
 export function createWorkerdHandler({
 	entrypoint,
@@ -15,21 +16,29 @@ export function createWorkerdHandler({
 }) {
 	console.log("create handler");
 
-	const viteHttpServerAddress = server.httpServer.address();
-	const viteServerAddress =
-		typeof viteHttpServerAddress === "string"
-			? viteHttpServerAddress
-			: `http://${viteHttpServerAddress.address}:${viteHttpServerAddress.port}`;
+	let mf: Miniflare | null;
 
-	const bootloader = workerdBootloader
-		.replace(/VITE_SERVER_ADDRESS/, viteServerAddress)
-		.replace(/WORKERD_APP_ENTRYPOINT/, entrypoint)
-		.replace(/GENERATE_RESPONSE/, frameworkRequestHandlingJs);
+	server.httpServer.once("listening", () => {
+		const viteHttpServerAddress = server.httpServer.address();
+		const viteServerAddress =
+			typeof viteHttpServerAddress === "string"
+				? viteHttpServerAddress
+				: `http://${
+						/^:*$/.test(viteHttpServerAddress.address)
+							? "localhost"
+							: viteHttpServerAddress.address
+				  }:${viteHttpServerAddress.port}`;
 
-	// create miniflare instance
-	// load it with module loader code and import to the entry point
+		const bootloader = workerdBootloader
+			.replace(/VITE_SERVER_ADDRESS/, viteServerAddress)
+			.replace(/WORKERD_APP_ENTRYPOINT/, entrypoint)
+			.replace(/GENERATE_RESPONSE/, frameworkRequestHandlingJs);
 
-	const mf = createMiniflareInstance({ script: bootloader });
+		// create miniflare instance
+		// load it with module loader code and import to the entry point
+
+		mf = createMiniflareInstance({ script: bootloader });
+	});
 
 	// this could be in the future replaced with websockets
 	server.middlewares.use(async (request, resp, next) => {
@@ -55,6 +64,12 @@ export function createWorkerdHandler({
 		let url = request.url.startsWith("/")
 			? `http://localhost${request.url}`
 			: request.url;
+
+		if (!mf) {
+			// this check is here as a precaution, it should never happen
+			// that at this point miniflare is not initialized!
+			throw new Error("miniflare not initialized!");
+		}
 
 		// TODO: we should support POST requests with body as well
 		//       for that we need something along the lines of
